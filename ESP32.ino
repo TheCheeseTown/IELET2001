525058f0bca70b4f05efb903bfc65c01fd279b30
TheCheeseTown <81992835+TheCheeseTown@users.noreply.github.com>
2022-11-07 16:11:20 +0100

    Update ESP32.ino

--- ESP32.ino
+++ ESP32.ino
@@ -13,14 +13,14 @@ Adafruit_BME280 bme;
  ****************************************/
 const char *UBIDOTS_TOKEN = ""; 
 const char *WIFI_SSID = "";   
-const char *WIFI_PASS = "";
-const char *SUBSCRIBE_DEVICE_LABEL = ""; //Navnet på den kanalen ubidots publiserer slider-verdiene til
+const char *WIFI_PASS = "";    
+const char *SUBSCRIBE_DEVICE_LABEL = "";   
 
-const int PUBLISH_FREQUENCY = 5000; // Hvor ofte skal det publiseres. 
+const int PUBLISH_FREQUENCY = 5000; // Update rate in millisecondsx
 
 unsigned long timer;
-uint8_t led = 25; //LED på pinne 25
-int moisturePin = 32; //Fuktighetssensor på pinne 32
+uint8_t led = 25;
+int moisturePin = 32; 
 
 Ubidots ubidots(UBIDOTS_TOKEN);
 
@@ -29,6 +29,7 @@ Ubidots ubidots(UBIDOTS_TOKEN);
  ****************************************/
 RTC_DATA_ATTR double tmpList[3]; 
 RTC_DATA_ATTR double moistureList[3];
+RTC_DATA_ATTR double AirmoistureList[3];
 RTC_DATA_ATTR byte count = 0;
 RTC_DATA_ATTR byte minTemp = 0;
 RTC_DATA_ATTR byte maxTemp = 0;
@@ -86,10 +87,12 @@ double ListToAverage(double array[],int lengde){
     int i;
     for (i=0;i<lengde;i++){
         sum += array[i];
+        Serial.println(array[i]);
     }
     //Serial.println(sum/lengde);
     return (sum/lengde);
 }
+
 
 // leser av sesnor flere ganger for å få en god avlesning
 float readMoisture() {
@@ -120,7 +123,7 @@ void setup()
 
   tmpList[count - 1] = bme.readTemperature(); // leser av temp fra BME280 og legger inn i liste
   moistureList[count - 1] = readMoisture(); // leser av verdie fra soilmoisture sensor
-
+  AirmoistureList[count - 1] = bme.readHumidity();
   // for hver n målinge skal esp32 koble seg på wifi og sende/motta verdier
   if (count == 3){ 
     ubidots.connectToWifi(WIFI_SSID, WIFI_PASS);
@@ -156,20 +159,24 @@ void loop()
   }
   if (int(millis() - timer) > PUBLISH_FREQUENCY ) // begynner å sende verdier etter 5 sekund
   {
-    double pubtemp = ListToAverage(tmpList, 2); // regner ut gjsn av temp målinger
-    double pubMoisture = ListToAverage(moistureList,2); // regner ut gjsn av soilmoisture målinger
+    double pubtemp = ListToAverage(tmpList, 3); // regner ut gjsn av temp målinger
+    double pubMoisture = ListToAverage(moistureList,3); // regner ut gjsn av soilmoisture målinger
     double pubMoistureProsent = map(pubMoisture,0,4095,0,100); // mapper gjsn av soilmoisture til prosent
-    float AIRHUMIDITY = bme.readHumidity(); // leser av luftfuktighet
+    float AIRHUMIDITY = ListToAverage(AirmoistureList,3); // leser av luftfuktighet
 
     // sjekker om veridene er innenfor de ønskede rammene
     MinMax(minTemp,maxTemp,pubtemp); 
     MinMax(minHumidity,maxHumidity,AIRHUMIDITY);
 
     // publiserer de ønskede verdiene til Ubidots
-    ubidots.add("tmp", pubtemp); //Emne den sender gitt verdi til
-    ubidots.add("moisture",pubMoistureProsent);
     ubidots.add("airmoisture",AIRHUMIDITY);
-    ubidots.publish();//Publiserer med MAC som "device label", dette gjør at hver enhet er unik. 
+    ubidots.publish();
+    ubidots.add("tmp", pubtemp); // Insert your variable Labels and the value to be sent
+    ubidots.publish();
+    ubidots.add("moisture",pubMoistureProsent);
+    ubidots.publish();
+    ubidots.add("airmoisture",AIRHUMIDITY);
+    ubidots.publish();
     delay(500);
   }
 
